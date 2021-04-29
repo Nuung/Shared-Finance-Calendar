c48718e347bc2582fe0316aad0759ba2f526b98f
Nuung <qlgks1@naver.com>
2021-04-30 02:39:28 +0900

    update schedule api

--- sfc-backend/src/routes/schedule.js
+++ sfc-backend/src/routes/schedule.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose'); // for mongoDB
+const ObjectID = require('mongodb').ObjectID; // for mongoDB's OID
 const axios = require("axios").default; // for bank API request
 const router = express.Router();
 
@@ -26,7 +27,7 @@ mongoose.connect(`mongodb://${dbConfig.username}:${dbConfig.password}@${dbConfig
 
 const User = require("../models/user");
 const Transaction = require("../models/transaction");
-const Excute = require("../models/transaction_excute");
+const Excute = require("../models/transaction_excute"); // alert_log와 상관 있습니다!
 const Schedule = require("../models/schedule");
 
 /** Add schedule!
@@ -53,34 +54,37 @@ router.post("/", function (req, res, next) {
 /** Get schedule by month and year!
  * @returns find 결과 array
  */
-router.get("/", function (req, res, next) {
+router.get("/:userId", function (req, res, next) {
 
-    // req.query
-    const targetYear = req.query.year;
-    const targetMonth = req.query.month;
+    // date filter
+    let rangeStart = new Date(`${Number(req.query.year)}-${Number(req.query.month)}`);
+    rangeStart.setDate(rangeStart.getDate() + 1);
+    const rangeEnd = new Date(`${Number(req.query.year)}-${Number(req.query.month) + 1}`);
 
-    Schedule.aggregate(
-        [{
-            $project: {
-                startTime: { $month: targetMonth }
-            }
-        }]
-    ).then(result => {
-        return res.status(201).json({ result });
-    }).catch(err => {
-        console.log(err);
-        return res.status(500).json({ err });
-    });
+    // createUserId (모임주), sharedUserId (array)에 현재 달력을 보고 있는 유저 ID이 있는것들 모두 불러와야함
+    Schedule
+        .find({
+            $or: [{ "startTime": { $gte: rangeStart, $lt: rangeEnd }, "createUserId": req.params.userId },
+            { "startTime": { $gte: rangeStart, $lt: rangeEnd }, "sharedUserId": { "$in": [req.params.userId] } }]
+        })
+        .then(result => {
 
-    // Schedule.find({
-    //     $expr: {
-    //         $and: [{ "$eq": [{ "$month": "$date" }, targetMonth] }, { "$eq": [{ "$year": "$date" }, targetYear] }]
-    //     }
-    // }).then(result => {
-    //     return res.status(201).json({ result });
-    // }).catch(err => {
-    //     return res.status(500).json({ err });
-    // });
+            return res.status(201).json({ result });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({ err });
+        });
+});
+
+
+/** Insert new alert_log
+ * @returns find by ObjectId and Updaet! 결과 return
+ */
+router.put("/alert", function (req, res, next) {
+    
+    // -> 
+
 
 });
 
