5b3b1713ceb39d3680ba06acd87db0f5541fa2f2
changmoomoon <public.stevemoon@gmail.com>
2021-04-30 12:32:29 +0900

    init frontend

--- sfc-frontend/src/App.js
+++ sfc-frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./normalize.css";
+
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
+import CalendarPage from "./pages/CalendarPage";
+import GroupInfoPage from "./pages/GroupInfoPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route component={CalendarPage} path={"/"} exact />
+      <Route component={LoginPage} path={"/login"} />
+      <Route component={RegisterPage} path={"/register"} />
+      <Route component={ProfilePage} path={"/profile"} />
+      <Route component={GroupInfoPage} path={"/group"} />
     </div>
   );
 }
