50a374c66d60f174b7f3a3686d1ea0739129133e
changmoomoon <public.stevemoon@gmail.com>
2021-05-02 22:04:01 +0900

    add calendar modal view

--- sfc-frontend/src/components/Header/Header.js
+++ sfc-frontend/src/components/Header/Header.js
@@ -7,7 +7,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <>
-      <div className="header">
+      <div className="header__">
         <div className="header__wrapper">
           <div className="header__hamburger">
             <Dropdown icon="align justify" size="large">
