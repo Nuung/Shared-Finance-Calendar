8f9ce4ad85ca6881971d0ecc5fdf2fd024b6aa97
Nuung <qlgks1@naver.com>
2021-05-02 18:44:01 +0900

    update front

---
+++ sfc-frontend/src/components/modal/Toast.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// status 가 0인 알람, 최근순 불러오기!! 
+const fetchAlertLog = () => {
+    toast("Wow so easy!");
+}
+
+const Toast = () => {
+
+    return (
+        <div>
+            <button onClick={fetchAlertLog}>Notify!</button>
+            <ToastContainer />
+        </div>
+    );
+};
+export default Toast;
--- sfc-frontend/src/pages/GroupInfoPage.js
+++ sfc-frontend/src/pages/GroupInfoPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ScheduleEndModal from "../components/modal/ScheduleEndModal";
+import Toast from "../components/modal/Toast";
 
 // 3. 캘린더에 등록한 일정 중에 모임이 끝나고 정산한 것만 / 월 별로
 const GroupInfoPage = () => {
   return (
-    <ScheduleEndModal />
+    <Toast />
   );
 };
-
 
 export default GroupInfoPage;
